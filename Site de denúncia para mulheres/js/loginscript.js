056fdae5d68c45f20d46c67e7bfe7e85ad79faaf
Gabriel Muniz <gabrielmuniz.si@outlook.com>
2024-06-22 20:24:24 -0300

    Delete Site de denúncia para mulheres directory

--- Site de denúncia para mulheres/js/loginscript.js
+++
@@ -1,9 +0,0 @@
-function logar(){
-    var login = document.getElementById("Usúario").value;
-    var senha = document.getElementById("Senha").value
-
-
-    if(Usúario == admin && Senha == "admin"){
-        alert("sucesso");
-    }
-}
